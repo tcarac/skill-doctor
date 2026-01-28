814f519ace11565fe7bc3b81a166775fb4475c9f
Tomas Caraccia <tomascaraccia@gmail.com>
2026-01-28 13:18:58 +0100

    fix: Fix Isort

--- src/skill_doctor/__init__.py
+++ src/skill_doctor/__init__.py
@@ -4,8 +4,8 @@ __version__ = "1.0.0"
 __author__ = "Tomas Caraccia"
 __license__ = "Apache-2.0"
 
-from .validator import ValidationResult, validate_skill
 from .models import SkillProperties
+from .validator import ValidationResult, validate_skill
 
 __all__ = [
     "validate_skill",
